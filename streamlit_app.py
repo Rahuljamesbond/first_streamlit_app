bd018b77fb58c53f9e6617567a6758dacbec0b20
Rahuljamesbond <119801574+Rahuljamesbond@users.noreply.github.com>
2023-01-02 01:08:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 import snowflake.connector
 streamlit.text("Hello from Snowflake:")
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
